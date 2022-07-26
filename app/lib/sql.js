d269dd141640816ac9bc7203f4e710217b4972a9
David <66866223+david-prv@users.noreply.github.com>
2022-07-26 19:14:16 +0200

    Update sql.js

--- app/lib/sql.js
+++ app/lib/sql.js
@@ -1,4 +1,12 @@
 class SQL {
+    /**
+     * Constructor
+     * 
+     * @param {string} host 
+     * @param {string} username 
+     * @param {string} password 
+     * @param {string} database 
+     */
     constructor(host, username, password, database) {
         this.host = host;
         this.username = username;
@@ -6,24 +14,52 @@ class SQL {
         this.database = database;
 
         this.mysql = require('mysql');
+        this.escape = require('sql-escape');
         this.connection = null;
     }
 
+    /**
+     * Open stateful connection
+     */
     connect() {
         this.connection = this.mysql.createConnection({
             host: this.host,
             user: this.username,
-            password: this.password
+            password: this.password,
+            database: this.database
         });
 
         this.connection.connect(function(err) {
             if(err) throw err;
-            console.log("MySQL connection was successful!");
         });
     }
 
+    /**
+     * Close connection
+     */
     disconnect() {
         this.connection.end();
+    }
+
+    /**
+     * 
+     * @param {string} string 
+     * @returns
+     */
+    serialize(string) {
+        if(typeof string !== "string") return null;
+        return this.escape(string);
+    }
+
+    /**
+     * 
+     * @param {string} query 
+     * @param {function} callback 
+     * @returns 
+     */
+    async exec(query, callback) {
+        if(typeof query !== "string" || typeof callback !== "function") return null;
+        return await this.connection.query(query, callback);
     }
 }
 
