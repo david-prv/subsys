75e2676bc0996bd47316813ec4aca81340b380b3
David <66866223+david-prv@users.noreply.github.com>
2022-07-26 12:21:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
  * Declarations
  */
 
+const crypto = require('crypto');
+const helmet = require('helmet');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const nonce = crypto.randomBytes(16).toString("base64");
 
 var routes = require('./app/routes');
 
@@ -13,10 +17,23 @@ var routes = require('./app/routes');
  * Express App Config
  */
 
+app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"], 
+          scriptSrc: ["'self'", 'https://cdn.jsdelivr.net', 'https://code.jquery.com', `'nonce-${nonce}'`],
+          styleSrc: ["'self'", 'https://fonts.googleapis.com', 'https://cdn.jsdelivr.net', "'unsafe-inline'"],
+        },
+      }
+    })
+);
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use('/', routes);
 app.use('/static', express.static(__dirname + '/static'));
+app.locals._nonce = nonce;
 
 /**
  * Run App
