5b79ee0e583ee93f522bb87616ea9184373b889a
David <66866223+david-prv@users.noreply.github.com>
2022-07-26 12:22:54 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -4,32 +4,36 @@
 
 const express = require('express');
 const dir = "../app/views";
+const bodyParser = require('body-parser');
+const csrf = require('csurf');
 
 var router = express.Router();
 var submissionController = require('./controller/submissionController');
 var identificationController = require('./controller/identificationController');
+var csrfProtection = csrf({ cookie: true });
+var parseForm = bodyParser.urlencoded({ extended: false });
 
 /**
  * Views
  */
 
-router.route('/').get(function(req, res) {
-    res.render(dir + '/index');
+router.route('/').get(csrfProtection, function(req, res) {
+    res.render(dir + '/index', {nonce: res.app.locals._nonce, csrf: req.csrfToken()});
 });
 
-router.route('/register').get(function(req, res) {
-    res.render(dir + '/register');
+router.route('/register').get(csrfProtection, function(req, res) {
+    res.render(dir + '/register', {nonce: res.app.locals._nonce, csrf: req.csrfToken()});
 });
 
 /**
  * Workflow
  */
 
-router.route('/flag').post(function(req, res) {
+router.route('/flag').post(parseForm, csrfProtection, function(req, res) {
     submissionController.submitFlag(req, res);
 });
 
-router.route('/register').post(function(req, res) {
+router.route('/register').post(parseForm, csrfProtection, function(req, res) {
     identificationController.registerId(req, res);
 });
 
